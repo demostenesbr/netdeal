f5161865d47bbfecd9878df6c2a04a6b358968bf
demostenesbr <demostenes@demostenes.com.br>
2023-10-27 00:25:25 -0300

    Feat: update and organize files on the sctructure

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,113 +1,60 @@
-$scope.treeData = [
-  {
-    id: 1,
-    name: "Richard Paul M.",
-    parentId: null,
-    checked: false,
-    children: [],
-  },
-  { id: 2, name: "Luis F. Doris", parentId: 1, checked: false, children: [] },
-  {
-    id: 3,
-    name: "Maurice Rudoff Ludwig",
-    parentId: 2,
-    checked: false,
-    children: [],
-  },
-  {
-    id: 4,
-    name: "Joseph E. James A.",
-    parentId: 3,
-    checked: false,
-    children: [],
-  },
-  { id: 5, name: "Alan G. William", parentId: 4, checked: false, children: [] },
-  {
-    id: 6,
-    name: "Guglielmo Hendrik Antoon",
-    parentId: 3,
-    checked: false,
-    children: [],
-  },
-  {
-    id: 7,
-    name: "Wladyslaw Stanislaw Ivar",
-    parentId: 3,
-    checked: false,
-    children: [],
-  },
-  { id: 8, name: "Élie Melvin", parentId: 3, checked: false, children: [] },
-  {
-    id: 9,
-    name: "Alvin E. Emil Theodor",
-    parentId: 3,
-    checked: false,
-    children: [],
-  },
-  {
-    id: 10,
-    name: "Helke Norman E.",
-    parentId: 3,
-    checked: false,
-    children: [],
-  },
-  {
-    id: 11,
-    name: "Kurt Dickinson W.",
-    parentId: 2,
-    checked: false,
-    children: [],
-  },
-  {
-    id: 12,
-    name: "Finn E. May-Britt",
-    parentId: 2,
-    checked: false,
-    children: [],
-  },
-  {
-    id: 13,
-    name: "James A. Shimon",
-    parentId: 2,
-    checked: false,
-    children: [],
-  },
-];
+var app = angular.module('treeApp', []);
+  app.controller('TreeController', function ($scope) {
+    // Initialize your tree data (you can load it from local storage here)
 
-var app = angular.module("myApp", []);
+    $scope.treeData = [
+      { id: 1, name: 'Richard Paul M.', parentId: null, checked: false, children: [] },
+      { id: 2, name: 'Luis F. Doris', parentId: 1, checked: false, children: [] },
+      { id: 3, name: 'Maurice Rudoff Ludwig', parentId: 2, checked: false, children: [] },
+      { id: 4, name: 'Luis F. Doris', parentId: 3, checked: false, children: [] },
+      { id: 5, name: 'Alan G. William', parentId: 4, checked: false, children: [] },
+      { id: 6, name: 'Guglielmo Hendrik Antoon', parentId: 3, checked: false, children: [] },
+      { id: 7, name: 'Wladyslaw Stanislaw Ivar', parentId: 3, checked: false, children: [] },
+      { id: 8, name: 'Élie Melvin', parentId: 3, checked: false, children: [] },
+      { id: 9, name: 'Alvin E. Emil Theodor', parentId: 3, checked: false, children: [] },
+      { id: 10, name: 'Helke Norman E.', parentId: 3, checked: false, children: [] },
+      { id: 11, name: 'Kurt Dickinson W.', parentId: 2, checked: false, children: [] },
+      { id: 12, name: 'Finn E. May-Britt', parentId: 2, checked: false, children: [] },
+      { id: 13, name: 'James A. Shimon', parentId: 2, checked: false, children: [] },
+    ];
 
-app.controller("ListController", function ($scope) {
-  $scope.items = [];
-  $scope.newItemName = "";
+    // Function to handle checkbox state changes
+    $scope.checkItem = function (item) {
+      updateChildrenState(item, item.checked);
+      updateParentState(item);
+    };
 
-  $scope.addItem = function () {
-    $scope.items.push({
-      name: $scope.newItemName,
-      checked: false,
-      subitems: [],
-    });
-    $scope.newItemName = "";
-  };
-
-  $scope.editItem = function (item) {
-    // Implement edit functionality
-  };
-
-  $scope.deleteItem = function (item) {
-    var index = $scope.items.indexOf(item);
-    if (index !== -1) {
-      $scope.items.splice(index, 1);
+    // Recursively update the state of all children
+    function updateChildrenState(item, state) {
+      item.checked = state;
+      angular.forEach(item.children, function (child) {
+        updateChildrenState(child, state);
+      });
     }
-  };
 
-  $scope.editSubitem = function (item, subitem) {
-    // Implement edit functionality for subitems
-  };
+    // Update the state of the parent based on children's state
+    function updateParentState(item) {
+      if (!item.parent) return;
 
-  $scope.deleteSubitem = function (item, subitem) {
-    var index = item.subitems.indexOf(subitem);
-    if (index !== -1) {
-      item.subitems.splice(index, 1);
+      var allChecked = true;
+      var allUnchecked = true;
+
+      angular.forEach(item.parent.children, function (child) {
+        if (child.checked) {
+          allUnchecked = false;
+        } else {
+          allChecked = false;
+        }
+      });
+
+      if (allChecked) {
+        item.parent.checked = true;
+      } else if (allUnchecked) {
+        item.parent.checked = false;
+      } else {
+        item.parent.checked = false; // Set to indeterminate
+      }
+
+      updateParentState(item.parent);
     }
-  };
-});
+  });
